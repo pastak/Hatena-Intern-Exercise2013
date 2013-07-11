804ae35f3d776a5dd7db2bfbaf3478e3bb6d736a
pastak <pasta0915@gmail.com>
2013-07-11 21:41:30 +0900

    finish js-1

--- js/main.js
+++ js/main.js
@@ -1,3 +1,35 @@
 // 課題 JS-1: 関数 `parseLTSVLog` を記述してください
 
+function parseLTSVLog(ltsv){
+	var line,feilds,field,ftmp,key,value;
+	var re_arr = [];
+	var log_lines = ltsv.split(/\n/);
+	for(var i = 0;i < log_lines.length;i++){
+		line = log_lines[i];
+		if(line === ''){
+				//空行なら飛ばす
+				continue;
+		};
+		re_arr[i] = {
+			path : '',
+			reqtime_microsec : 0
+		};
+		feilds = line.split(/\t/);
+		for(var j = 0;j < feilds.length;j++){
+			feild = feilds[j];
+			ftmp = feild.split(/:/);
+			key = ftmp[0];
+			value = ftmp[1];
+			if(key === 'path'){
+				re_arr[i].path = value;
+			}else if(key === 'reqtime_microsec'){
+				re_arr[i].reqtime_microsec = value-0;
+			}else{
+				
+			}
+		}
+	}
+	return re_arr;
+}
+
 // 課題 JS-2: 関数 `createLogTable` を記述してください
