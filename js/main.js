3b48da2d2a380983e533ebdd133d509c9746c434
pastak <pasta0915@gmail.com>
2013-07-11 22:22:05 +0900

    finish js-2

--- js/main.js
+++ js/main.js
@@ -33,3 +33,34 @@ function parseLTSVLog(ltsv){
 }
 
 // 課題 JS-2: 関数 `createLogTable` を記述してください
+function createLogTable(divElm,logArr){
+	var table = document.createElement('table');
+	//テーブルのヘッダー関係の要素を生成
+	var thead = document.createElement('thead');
+	var thead_tr = document.createElement('tr');
+	var thead_th1 = document.createElement('th');
+	thead_th1.textContent = 'path';
+	var thead_th2 = document.createElement('th');
+	thead_th2.textContent = 'reqtime_microsec';
+	//theadに追加する
+	thead_tr.appendChild(thead_th1);
+	thead_tr.appendChild(thead_th2);
+	thead.appendChild(thead_tr);
+	table.appendChild(thead);
+	//テーブルの本体に入れる要素を生成
+	var tbody = document.createElement('tbody');
+	var tr,td1,td2;
+	for(var i = 0;i < logArr.length;i++){
+		tr = document.createElement('tr');
+		td1 = document.createElement('td');
+		td1.textContent = logArr[i].path;
+		td2 = document.createElement('td');
+		td2.textContent = logArr[i].reqtime_microsec;
+		tr.appendChild(td1);
+		tr.appendChild(td2);
+		tbody.appendChild(tr);
+	}
+	table.appendChild(tbody);
+	console.log(table);
+	divElm.appendChild(table);
+}
